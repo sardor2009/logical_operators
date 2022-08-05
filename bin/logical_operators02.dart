9377d55fb47d00a7fcbd337dece6fc9eabb32f6d
sardor <ruziqulovsardor3@gmail.com>
2022-08-05 13:36:41 +0500

    code

--- bin/logical_operators02.dart
+++ bin/logical_operators02.dart
@@ -9,9 +9,15 @@ Given two integers a, b,  check the following statement "Each of the numbers 'a'
         bool: answer
 */
 bool func(int a, int b){
-return a > 0 && b < 0;
+if( a > 0 && b > 0){
+    return true;
+}
+if(a > 0 && b < 0){
+    return false;
+}
+return false;
 }
 
 void main() {
-    print(func(4, -2));
+    print(func(-4, -2));
 }
